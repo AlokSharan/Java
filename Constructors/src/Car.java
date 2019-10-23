15f0a966f76fa30f1ac057a7f1f45e6daff1eeec
admin <admin@192.168.1.2>
2019-10-23 16:34:19 +0530

    Constructor

---
+++ Constructors/src/Car.java
@@ -0,0 +1,23 @@
+// You can have many Parameter as you want.
+
+
+public class Car {
+	int modelYear;
+	String modelName;
+	
+	
+	public Car(int Year , String Name) {
+		modelYear = Year;
+		modelName = Name;
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Car car = new Car(2000, "Duster");
+		System.out.println(car.modelYear + " " + car.modelName);
+		
+	}
+
+}
---
+++ Constructors/src/MyConstructor.java
@@ -0,0 +1,23 @@
+/*
+ 
+Create a Constructor
+
+*/
+
+
+public class MyConstructor {
+	int x;						//Create a Class Attributes
+	
+	public MyConstructor() {	//Create a Class Constructor same as Class name
+		x = 5;					//Set the initial value for the Class Attribute x
+			
+	}
+	
+	public static void main(String[] args) {
+		
+		MyConstructor myObj = new MyConstructor();		//Create Object(This will Call the Constructor)
+		
+		System.out.println(myObj.x);				//Print the value of x
+	}
+
+}
