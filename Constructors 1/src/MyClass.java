fff36d9d483bb0b9779ab731667d4b9e89611c75
AlokSharan <sharanalok5@gmail.com>
2019-11-06 17:00:54 +0530

    new constructor create

---
+++ Constructors 1/src/MyClass.java
@@ -0,0 +1,15 @@
+
+
+class MyClass{
+	
+	public static void main(String[] args) {
+		
+		Person myObj = new Person();
+		
+		System.out.println("Name-> " + myObj.fname + " " + myObj.lname);
+		System.out.println("Email_Id-> " + myObj.email_id);
+		System.out.println("Address-> " + myObj.address);
+		System.out.println("Age-> " + myObj.age);
+		
+	}
+}
---
+++ Constructors 1/src/Person.java
@@ -0,0 +1,12 @@
+
+public class Person {
+	
+	String fname = "Alok";
+	String lname = "Sharan";
+	String email_id = "alok_it@hotmail.com";
+	String address = "Ambedkar Nagar";
+	int age = 30;
+	
+	
+	
+}
